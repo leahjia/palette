c5cc8dfd9a24fc5615cdf7a5e0c98c203e40226e
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-09 21:38:54 -0800

    some colors going

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use colorgrad::{Color, CustomGradient};
 use palette::{FromColor, Lch, Srgb};
 
 fn main() {
@@ -11,9 +12,24 @@ fn main() {
 }
 
 #[tauri::command]
-fn generate_gradient(r: u8, g: u8, b: u8) {
+fn generate_gradient(r: u8, g: u8, b: u8) -> Vec<Vec<u8>> {
     println!("Yo I got something: {}, {}, {}", r, g, b);
-    let rgb = Srgb::new(r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0);
-    // let lch: Lch = Lch::from_color(rgb.into_linear());
-    // let gradient = Gradient::new(vec![Lch::new(0, lch.chroma as i32, lch.hue), lch, Lch::new(1, lch.chroma as i32, lch.hue)]);
+    let my_rgb = Srgb::new(r as f64 / 255.0, g as f64 / 255.0, b as f64 / 255.0);
+
+    let my_lch = Lch::from_color(my_rgb.into_linear());
+    dbg!(my_lch);
+
+    let gradient = CustomGradient::new()
+        .colors(&[
+            Color::from_rgb(0.0, my_lch.chroma, my_lch.hue.into()),
+            Color::from_rgb(my_lch.l, my_lch.chroma, my_lch.hue.into()),
+            Color::from_rgb(128.0, my_lch.chroma, my_lch.hue.into()),
+        ])
+        .build()
+        .unwrap();
+
+    let colors = gradient.colors(10).into_iter().map(|c| vec![c.r as u8, c.g as u8, c.b as u8]).collect::<Vec<_>>();
+
+    dbg!(&colors);
+    colors
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { invoke } from "@tauri-apps/api";
 
 function App() {
     const [color, setColor] = useState("#5b95b5")
+    const [gradient, setGradient] = useState([])
 
     return <div>
         <SketchPicker
@@ -12,10 +13,22 @@ function App() {
             onChange={(color, evt) => {
                 setColor(color);
                 console.log(color);
-                invoke("generate_gradient", color.rgb);
+                invoke("generate_gradient", color.rgb).then((res) => {
+                    console.log("res: " + res);
+                    setGradient(res);
+                });
             }}
         />
+        <div className="gradient">
+            {gradient.map((color) => {
+                return <div style={{
+                    padding: "1rem",
+                    backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+                }}>
+                    rgb({color[0]}, {color[1]}, {color[2]})
+                </div>
+            })}
+        </div>
     </div>
 }
-
 export default App;
